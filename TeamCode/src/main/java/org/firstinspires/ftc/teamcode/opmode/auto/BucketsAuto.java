54097d35f421b6329c5663a4436b41841962357f
JackLu09SBS <jackclu909@gmail.com>
2025-03-13 23:02:47 -0400

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BucketsAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BucketsAuto.java
@@ -56,7 +56,7 @@ public class BucketsAuto extends LinearOpMode {
                 .waitSeconds(30)
 
                 .setTangent(Math.toRadians(180))
-                .splineToLinearHeading(new Pose2d(50.8, 38, Math.toRadians(-90)), Math.toRadians(-90))
+                .splineToLinearHeading(new Pose2d(50.8, 45, Math.toRadians(-90)), Math.toRadians(-90))
 
                 // .turn(Math.toRadians(45))
                 // .strafeTo(new Vector2d(48,46))
@@ -77,7 +77,7 @@ public class BucketsAuto extends LinearOpMode {
 
                 .waitSeconds(0.75)
                 .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(new Pose2d(60.5, 38, Math.toRadians(-90)), Math.toRadians(-90))
+                .splineToLinearHeading(new Pose2d(60.5, 45, Math.toRadians(-90)), Math.toRadians(-90))
                 //  .waitSeconds(1)
                 // .afterTime(0.5, () -> scoringMechanism.setPosition(ScoringMechanismPosition.DEPOSIT))
                 .waitSeconds(0.75)
@@ -93,7 +93,7 @@ public class BucketsAuto extends LinearOpMode {
                 */
                 .waitSeconds(0.75)
                 .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(new Pose2d(58.5, 34, Math.toRadians(-45)), -Math.PI / 4)
+                .splineToLinearHeading(new Pose2d(54, 40.5, Math.toRadians(-40)), -Math.PI / 4)
                 // .waitSeconds(1)
                 // .afterTime(0.5, () -> scoringMechanism.setPosition(ScoringMechanismPosition.DEPOSIT))
                 .waitSeconds(0.75)
