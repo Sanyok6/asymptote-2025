8cdbf529b6b06c4d371d91cd9d99d86679b206d8
JackLu09SBS <jackclu909@gmail.com>
2025-03-15 15:13:37 -0400

    experimental autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/FIVEPIECEBUCKETS.java
@@ -0,0 +1,189 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+// RR-specific imports
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.PinpointDrive;
+import org.firstinspires.ftc.teamcode.subsystems.ScoringMechanism;
+import org.firstinspires.ftc.teamcode.subsystems.ScoringMechanismPosition;
+
+// RR-specific imports
+
+// Non-RR imports
+
+
+@Config
+@Autonomous(name = "5PIECESBUCKET", group = "Autonomous")
+public class FIVEPIECEBUCKETS extends LinearOpMode {
+    public void runOpMode() {
+        // instantiate your MecanumDrive at a particular pose.
+
+        Pose2d beginPose = new Pose2d(32.8, 61, Math.toRadians(-180));
+        PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
+        ScoringMechanism scoringMechanism = new ScoringMechanism(hardwareMap);
+
+        scoringMechanism.deposit.closeClaw();
+
+        Action traj = drive.actionBuilder(beginPose)
+
+                // PLACE PRELOADED SAMPLE
+
+                .stopAndAdd(() -> scoringMechanism.setPosition(ScoringMechanismPosition.DEPOSIT))
+                .waitSeconds(0.75)
+                .setTangent(0)
+                .splineToLinearHeading(new Pose2d(56, 50, Math.toRadians(-135)), Math.toRadians(45))
+
+                .stopAndAdd(() -> {
+                    scoringMechanism.deposit.openClaw();
+                    scoringMechanism.update();
+                })
+                .waitSeconds(0.25)
+
+
+                // PICKUP FIRST SAMPLE
+
+                .stopAndAdd( () -> {
+                    scoringMechanism.horizontalSlide.setTargetPosition(300);
+                    scoringMechanism.setPosition(ScoringMechanismPosition.INTAKE_LOWER);
+                    scoringMechanism.intake.runIntake();
+                    scoringMechanism.update();
+                })
+
+                .setTangent(Math.toRadians(180))
+                .splineToLinearHeading(new Pose2d(49.3, 43.25, Math.toRadians(-90)), Math.toRadians(-90))
+
+                .waitSeconds(0.5)
+                .stopAndAdd(() -> {
+                    scoringMechanism.setPosition(ScoringMechanismPosition.TRANSFER);
+                })
+                .waitSeconds(3)
+
+
+                // PLACE FIRST SAMPLE
+
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(57.5, 51.5, Math.toRadians(-135)), Math.toRadians(45))
+                .stopAndAdd(() -> {
+                    scoringMechanism.deposit.openClaw();
+                    scoringMechanism.update();
+                })
+                .waitSeconds(0.36)
+
+
+                // PICKUP SECOND SAMPLE
+
+                .stopAndAdd(() -> {
+                    scoringMechanism.horizontalSlide.setTargetPosition(250);
+                    scoringMechanism.setPosition(ScoringMechanismPosition.INTAKE_LOWER);
+                    scoringMechanism.intake.runIntake();
+                    scoringMechanism.update();
+                })
+
+                .setTangent(Math.toRadians(-90))
+                .splineToLinearHeading(new Pose2d(60.5, 43.5, Math.toRadians(-70)), Math.toRadians(-90))
+
+                .waitSeconds(0.5)
+                .stopAndAdd(() -> {
+                    scoringMechanism.setPosition(ScoringMechanismPosition.TRANSFER);
+                })
+                .waitSeconds(3)
+
+
+                // PLACE SECOND SAMPLE
+
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(57.5, 52, Math.toRadians(-135)), Math.toRadians(45))
+                .stopAndAdd(() -> {
+                    scoringMechanism.deposit.openClaw();
+                    scoringMechanism.update();
+                })
+                .waitSeconds(0.25)
+
+
+                // PICKUP THIRD SAMPLE
+
+                .afterTime(0.25, () -> {
+                    scoringMechanism.horizontalSlide.setTargetPosition(150);
+                    scoringMechanism.setPosition(ScoringMechanismPosition.INTAKE_LOWER);
+                    scoringMechanism.intake.runIntake();
+                    scoringMechanism.update();
+                })
+
+                .setTangent(Math.toRadians(-90))
+                .splineToLinearHeading(new Pose2d(55, 36, Math.toRadians(-35)), -Math.PI/4)
+
+                .waitSeconds(1)
+                .stopAndAdd(() -> {
+                    scoringMechanism.setPosition(ScoringMechanismPosition.TRANSFER);
+                })
+                .waitSeconds(3)
+
+
+                // PLACE THIRD SAMPLE
+
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(57.5, 52, Math.toRadians(-135)), Math.toRadians(55))
+                .stopAndAdd(() -> {
+                    scoringMechanism.deposit.openClaw();
+                    scoringMechanism.update();
+                })
+                .waitSeconds(0.25)
+                // PICKUP FOURTH SAMPLE
+
+                .splineToLinearHeading(new Pose2d(20,38 , Math.toRadians(180)), Math.toRadians(180))
+                .setTangent(Math.toRadians(182.5))
+                .afterTime(0.25, () -> {
+                    scoringMechanism.horizontalSlide.setTargetPosition(150);
+                    scoringMechanism.setPosition(ScoringMechanismPosition.INTAKE_LOWER);
+                    scoringMechanism.intake.runIntake();
+                    scoringMechanism.update();
+                })
+                .splineToLinearHeading(new Pose2d(-48,48 , Math.toRadians(90)), Math.toRadians(135))
+                .waitSeconds(1)
+                .stopAndAdd(() -> {
+                    scoringMechanism.setPosition(ScoringMechanismPosition.TRANSFER);
+                })
+                .waitSeconds(3)
+                .setTangent(Math.toRadians(-45))
+                .splineToLinearHeading(new Pose2d(20,38 , Math.toRadians(180)), Math.toRadians(0))
+                .setTangent(Math.toRadians(15))
+                // PLACE FOURTH SAMPLE
+                .splineToLinearHeading(new Pose2d(57.5, 52, Math.toRadians(-135)), Math.toRadians(45))
+                .stopAndAdd(() -> {
+                    scoringMechanism.deposit.openClaw();
+                    scoringMechanism.update();
+                })
+                .waitSeconds(0.25)
+
+//                .waitSeconds(30)
+
+
+                // PARK
+
+                /*      .waitSeconds(0.75)
+                      .setTangent(Math.toRadians(225))
+                      .splineToLinearHeading(new Pose2d(34, 0, Math.toRadians(180)), Math.PI) //park
+      */
+                .build();
+
+
+        waitForStart();
+
+        Actions.runBlocking(
+                new ParallelAction(
+                        traj,
+                        (t) -> {scoringMechanism.update(); return true;}
+                )
+        );
+    }
+
+
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/NoBuckets2Piece.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+// RR-specific imports
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.PinpointDrive;
+import org.firstinspires.ftc.teamcode.subsystems.ScoringMechanism;
+
+
+@Config
+@Autonomous(name = "NoBucketsAuto", group = "Autonomous")
+public class NoBuckets2Piece extends LinearOpMode {
+    public void runOpMode() {
+        // instantiate your MecanumDrive at a particular pose.
+
+        ScoringMechanism scoringMechanism = new ScoringMechanism(hardwareMap);
+
+        Pose2d beginPose = new Pose2d(-8.8, 61.4, Math.toRadians(180));
+        PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
+        waitForStart();
+
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .setTangent(-Math.PI/2)
+                        .splineToLinearHeading(new Pose2d(-9,40,Math.PI/2), -Math.PI/2)
+                        // .strafeTo(new Vector2d(-9,38))
+                        .setTangent(Math.toRadians(-90))
+                        .lineToY(34)
+                        .waitSeconds(0.5)
+                        .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(-48,51,Math.PI/2), Math.PI/2)
+                        .waitSeconds(0.5)
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-9,40,Math.PI/2), 0)
+                        .setTangent(Math.toRadians(-90))
+                        .lineToY(34)
+                        .waitSeconds(0.5)
+                        .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(-36,30,Math.PI/2), -Math.PI/2)
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-48,16,Math.PI/2), Math.PI)
+                        .setTangent(Math.PI/2)
+                        .lineToY(47)
+                        .setTangent(-Math.PI/2)
+                        .splineToLinearHeading(new Pose2d(-62,12,Math.PI/2), -Math.PI)
+                        .setTangent(Math.PI/2)
+                        .lineToY(47)
+                        .splineToLinearHeading(new Pose2d(-48,51,Math.PI/2), Math.PI/2)
+                        .setTangent(Math.toRadians(-45))
+                        .splineToConstantHeading(new Vector2d(-8, 36), -Math.PI / 2) //changed to a little higher?
+                        .setTangent(Math.toRadians(-90))
+                        .lineToY(34)
+                        .waitSeconds(0.5)
+                        .setTangent(Math.toRadians(135))
+                        .splineToLinearHeading(new Pose2d(-48, 51, Math.PI/2), Math.PI/2)
+                        .setTangent(Math.toRadians(-45))
+                        .splineToConstantHeading(new Vector2d(-8, 36), -Math.PI / 2) //changed to a little higher?
+                        .setTangent(Math.toRadians(-90))
+                        .lineToY(34)
+                        .waitSeconds(0.5)
+                        .setTangent(Math.toRadians(135))
+                        .splineToLinearHeading(new Pose2d(-48, 51, Math.PI/2), Math.PI/2)
+
+                        //    .setTangent(-Math.PI/2)
+                        .build());
+
+
+
+             /* Parking submersible
+             .setTangent(Math.PI)
+                .lineToX(-32)
+                .splineToLinearHeading(new Pose2d(-32, 0, Math.PI/2), -Math.PI / 4)
+*/
+
+    }
+}
